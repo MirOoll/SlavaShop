60a4ecd6d606d0b36b54599e1083e3e015430a1b
MirOoll <72094073+RuslanVoloshin-Azur@users.noreply.github.com>
2021-04-01 10:53:29 +0300

    orig

---
+++ Assets/Scripts/RandomMovementObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovementObject : MonoBehaviour
+{
+
+    public GameObject[] childrenObjects;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator delayChangeMovement()
+    {
+        yield return new WaitForSeconds(2f);
+    }
+
+    private void startRandomMoving()
+    {
+        getRandomMovement();
+    }
+    private void getRandomMovement()
+    {
+        
+        Vector3 targetPosition = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f) * Time.deltaTime);
+        rb.MovePosition(rb.position + targetPosition);
+        //StartCoroutine(delayChangeMovement());
+        //getRandomMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        getRandomMovement();
+    }
+}
